65b798214938b11300a52f10a54716c7fcc19652
psh2003 <83701900+psh2003@users.noreply.github.com>
2023-10-02 23:47:02 +0900

    first commit

---
+++ src/main/java/com/pbl/pt/PtManagementApplication.java
@@ -0,0 +1,13 @@
+package com.pbl.pt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pbl/pt/PtManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbl.pt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
